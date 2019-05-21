2915eae48d06577c275aa65bddaed92de8360204
Skylar Wurster <swwurster@gmail.com>
2019-05-20 20:10:34 -0400

    Cleans up code

--- Solution3/Code/classifier.py
+++ Solution3/Code/classifier.py
@@ -18,64 +18,44 @@ normal_images = '../Images/NORMAL'
 codes_path = './codes'
 labels_path = './labels'
 
-class_0 = cancer_images
+class_0 = contralateral_cancer_images
 class_1 = normal_images
 
-#print(contents)
-
-codes_list = []
-codes = None
-labels = []
-images = []
+images, labels = utility_functions.loadImagesFromDir((class_0, class_1), (0,1))
+print("Images: " + str(np.shape(images)))
+print("Labels: " + str(np.shape(labels)))
 
 sess = tf.Session()
 print("Session start")
+
 vgg = vgg19.Vgg19()
 input_ = tf.placeholder(tf.float32, [None, 224, 224, 3])
 with tf.name_scope("content_vgg"):
     vgg.build(input_)
-
-images, labels = utility_functions.loadImagesFromDir((class_0, class_1), (0,1))
-
 # Get the values from the relu6 layer of the VGG network
 feed_dict = {input_: images}
-codes_batch = sess.run(vgg.relu6, feed_dict=feed_dict)
+codes = sess.run(vgg.relu6, feed_dict=feed_dict)
 sess.close()
 
-# Build an array of codes
-if codes is None:
-    codes = codes_batch
-else:
-    codes = np.append(codes_batch, codes, axis=0)
+np.save(codes_path, codes) 
+np.save(labels_path, labels) 
 
-
-
-np.save(codes_path, codes)  # user input
-#print("Shape of codes")
-#print(np.shape(codes))
-np.save(labels_path, labels)  # user input
-#print("Shape of labels")
-#print(np.shape(labels))
-#print("Labels: " + str(labels))
 pickle.dump(codes, open('codes', 'wb'))
 pickle.dump(labels, open('labels', 'wb'))
+print("Codes loaded: " + str(np.shape(codes)))
+print("Labels loaded: " + str(np.shape(labels)))
 
 X_train, X_test, y_train, y_test = train_test_split(codes, labels, test_size=0.2, random_state=0)
-#print(y_train)
-#print(X_test)
 
 clf = LinearSVC(C=0.0001)
-# clf = CalibratedClassifierCV(clf)
 clf.fit(X_train, y_train)
 
 print("Confidence values")
 print(clf.decision_function(X_test))
 
-# print("Prediction probabilities")
-# print(clf.predict_proba(X_test))
-
 print("Predictions")
 print(clf.predict(X_test))
+
 print("Actual values")
 print(y_test)
 
--- Solution3/Code/classifier_auc.py
+++ Solution3/Code/classifier_auc.py
@@ -1,6 +1,7 @@
+import sys
+sys.path.insert(0, '/tensorflowvgg')
 import os
 import pickle
-import argparse
 from os.path import isfile, isdir
 import numpy as np
 import tensorflow as tf
@@ -8,130 +9,60 @@ from keras.preprocessing.image import load_img, img_to_array
 from sklearn.svm import SVC, LinearSVC
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix, auc, roc_curve
+import tensorflowvgg.vgg19 as vgg19
+import utility_functions
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
-from tensorflow_vgg import vgg19
 
-def get_args():
-    parser = argparse.ArgumentParser(
-        description='Script classifies images in data directory'
-    )
-    parser.add_argument(
-        '-d', '--data', type=str, help='Directory containing image data', required=True
-    )
-    parser.add_argument(
-        '-c', '--codes', type=str, help='Output file path for feature vector', required=True
-    )
-    parser.add_argument(
-        '-l', '--labels', type=str, help='Output file path for labels', required=True
-    )
-    args = parser.parse_args()
-    dataset_folder_path = args.data
-    codes_path = args.codes
-    labels_path = args.labels
-    return dataset_folder_path, codes_path, labels_path
+cancer_images = '../Images/CANCER'
+contralateral_cancer_images = '../Images/CONTRALATERAL BREAST TO CANCEROUS'
+normal_images = '../Images/NORMAL'
+codes_path = './codes'
+labels_path = './labels'
 
-dataset_folder_path, codes_path, labels_path = get_args()
+class_0 = cancer_images
+class_1 = normal_images
 
-vgg_dir = '../mammogist2/tensorflow_vgg'  # modify
-# Make sure vgg exists
-if not isdir(vgg_dir):
-    raise Exception("VGG directory doesn't exist!")
-
-data_dir = dataset_folder_path
-contents = os.listdir(dataset_folder_path)
-print(contents)
-
-batch_size = 150
-codes_list = []
-codes = None
-labels = []
-batch = []
+images, labels = utility_functions.loadImagesFromDir((class_0, class_1), (0,1))
+print("Images: " + str(np.shape(images)))
+print("Labels: " + str(np.shape(labels)))
 
 sess = tf.Session()
 print("Session start")
+
 vgg = vgg19.Vgg19()
 input_ = tf.placeholder(tf.float32, [None, 224, 224, 3])
 with tf.name_scope("content_vgg"):
     vgg.build(input_)
-
-files = os.listdir(dataset_folder_path)
-print(files)
-
-for i, file in enumerate(files, 1):
-    # Add images to the current batch
-    print(i)
-    file_name = os.path.join(dataset_folder_path, file)
-    print(file_name)
-    img = load_img(file_name, target_size=(224, 224))
-    img_arr = img_to_array(img)
-    print("Shape of imgarr:")
-    print(img_arr.shape)
-    batch.append(img_arr)  # change according to original script
-    print(np.shape(batch))
-
-    if file.find("AC") == -1:
-        labels.append(0)
-    else:
-        labels.append(1)
-
-    # Run batch through the network to get codes
-    if i % batch_size == 0 or i == len(files):
-        # Image batch to pass to VGG network
-        images = batch
-
-        # Get the values from the relu6 layer of the VGG network
-        feed_dict = {input_: images}
-        print("Batch shape:")
-        print(np.shape(images))
-        codes_batch = sess.run(vgg.relu6, feed_dict=feed_dict)
-
-        # Build an array of codes
-        if codes is None:
-            codes = codes_batch
-        else:
-            codes = np.append(codes, codes_batch, axis=0)
-
-        # Reset to build next batch
-        batch = []
-        print('{} images processed'.format(i))
-
+# Get the values from the relu6 layer of the VGG network
+feed_dict = {input_: images}
+codes = sess.run(vgg.relu6, feed_dict=feed_dict)
 sess.close()
 
-np.save(codes_path, codes)  # user input
-print("Shape of codes")
-print(np.shape(codes))
-np.save(labels_path, labels)  # user input
-print("Shape of labels")
-print(np.shape(labels))
+np.save(codes_path, codes) 
+np.save(labels_path, labels) 
 
 pickle.dump(codes, open('codes', 'wb'))
 pickle.dump(labels, open('labels', 'wb'))
 
 X_train, X_test, y_train, y_test = train_test_split(codes, labels, test_size=0.2, random_state=0)
-print(y_train)
-print(X_test)
 
 clf = LinearSVC(C=0.0001)
-# clf = CalibratedClassifierCV(clf)
 clf.fit(X_train, y_train)
 
 print("Confidence values")
 print(clf.decision_function(X_test))
 
-# print("Prediction probabilities")
-# print(clf.predict_proba(X_test))
-
 print("Predictions")
 print(clf.predict(X_test))
+
 print("Actual values")
 print(y_test)
 
 # Plot ROC curve
 actual = y_test
 predictions = clf.decision_function(X_test)
-# predictions = clf.predict(X_test)
 
 fpr, tpr, thresholds = roc_curve(actual, predictions)
 roc_auc = auc(fpr, tpr)
